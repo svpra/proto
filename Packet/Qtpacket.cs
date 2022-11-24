a63b9f1cd336a9d1f317bf476783f68efe1899b2
Andrew <93141508+svpra@users.noreply.github.com>
2022-11-24 22:52:07 +0300

    Update Qtpacket.cs

--- Packet/Qtpacket.cs
+++ Packet/Qtpacket.cs
@@ -1,6 +1,12 @@
-public class QtPacket
-{
-    public byte PacketType { get; private set; }
-    public byte PacketSubtype { get; private set; }
-    public List<QtPacketField> Fields { get; set; } = new List<QtPacketField>();
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace proto {
+    public class QtPacket
+    {
+        public byte PacketType { get; private set; }
+        public byte PacketSubtype { get; private set; }
+        public List<QtPacketField> Fields { get; set; } = new List<QtPacketField>();
+    }
 }
