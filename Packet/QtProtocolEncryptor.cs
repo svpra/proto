dd6eb0fdefae2bc4d7a69a030a68c922d2ad1c6d
Andrew <93141508+svpra@users.noreply.github.com>
2022-12-24 23:15:49 +0300

    Create QtProtocolEncryptor.cs

---
+++ Packet/QtProtocolEncryptor.cs
@@ -0,0 +1,17 @@
+namespace QtProtocol
+{
+    public class QtProtocolEncryptor
+    {
+        private static string Key { get; } = "2e985f930";
+
+        public static byte[] Encrypt(byte[] data)
+        {
+            return RijndaelHandler.Encrypt(data, Key);
+        }
+
+        public static byte[] Decrypt(byte[] data)
+        {
+            return RijndaelHandler.Decrypt(data, Key);
+        }
+    }
+}
